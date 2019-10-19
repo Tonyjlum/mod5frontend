d497a7f8bd6b6225c3d482bf4a599ebd07356e97
Tonyjlum <tonyjlum@gmail.com>
2019-10-19 11:18:27 -0400

    Updates

---
+++ precache-manifest.b48f72e208b95d864bfa1713da139ad9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9327f6c1f7976320e907",
+    "url": "/Tonyjlum/GitPagesDeployHelpingHand.git/static/css/main.466a0bce.chunk.css"
+  },
+  {
+    "revision": "9327f6c1f7976320e907",
+    "url": "/Tonyjlum/GitPagesDeployHelpingHand.git/static/js/main.9327f6c1.chunk.js"
+  },
+  {
+    "revision": "722f40926535c53e3d06",
+    "url": "/Tonyjlum/GitPagesDeployHelpingHand.git/static/js/runtime~main.722f4092.js"
+  },
+  {
+    "revision": "c4bdce8b3b8eb70fd63d",
+    "url": "/Tonyjlum/GitPagesDeployHelpingHand.git/static/css/2.5ce357e6.chunk.css"
+  },
+  {
+    "revision": "c4bdce8b3b8eb70fd63d",
+    "url": "/Tonyjlum/GitPagesDeployHelpingHand.git/static/js/2.c4bdce8b.chunk.js"
+  },
+  {
+    "revision": "3cdc9879d964ad0f952ff280a02b013b",
+    "url": "/Tonyjlum/GitPagesDeployHelpingHand.git/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Helping-Hand-App/precache-manifest.95ddc3e11278be3e8e914a18fed390b3.js"
+  "/Tonyjlum/GitPagesDeployHelpingHand.git/precache-manifest.b48f72e208b95d864bfa1713da139ad9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Helping-Hand-App/index.html", {
+workbox.routing.registerNavigationRoute("/Tonyjlum/GitPagesDeployHelpingHand.git/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
