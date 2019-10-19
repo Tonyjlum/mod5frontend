054cb1515da609ad9d5db3afb0599b7bc2a4159a
Tonyjlum <tonyjlum@gmail.com>
2019-10-19 11:40:51 -0400

    Updates

---
+++ precache-manifest.f205525276d1a09e02fcd12a44cbc947.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "db7b362815aadcf2d57b",
+    "url": "/Helping-Hand-App/public/index.html/static/css/main.466a0bce.chunk.css"
+  },
+  {
+    "revision": "db7b362815aadcf2d57b",
+    "url": "/Helping-Hand-App/public/index.html/static/js/main.db7b3628.chunk.js"
+  },
+  {
+    "revision": "7114560cfb627392d12e",
+    "url": "/Helping-Hand-App/public/index.html/static/js/runtime~main.7114560c.js"
+  },
+  {
+    "revision": "c4bdce8b3b8eb70fd63d",
+    "url": "/Helping-Hand-App/public/index.html/static/css/2.5ce357e6.chunk.css"
+  },
+  {
+    "revision": "c4bdce8b3b8eb70fd63d",
+    "url": "/Helping-Hand-App/public/index.html/static/js/2.c4bdce8b.chunk.js"
+  },
+  {
+    "revision": "badaaa3a2d768fd0baa071e3ad449cea",
+    "url": "/Helping-Hand-App/public/index.html/index.html"
+  }
+];
